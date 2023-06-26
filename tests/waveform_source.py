74727c601176f10cac83d0d065558170de5e5bae
Stefano Rasi <rasi.stefano@gmail.com>
2023-06-26 19:03:38 +0200

    Clean up

--- tests/waveform_source.py
+++ tests/waveform_source.py
@@ -1,5 +1,4 @@
 import sys
-import time
 
 sys.path.insert(0, '')
 
@@ -11,10 +10,11 @@ from harmonics_waveform import HarmonicsWaveform
 DEVICE = 4
 
 input = KeyboardInput()
+
 output = AudioOutput(DEVICE)
 
 waveform = HarmonicsWaveform([0.1, 0.04, 0.02])
 
 source = WaveformSource(waveform, input, output)
 
-output.start_stream()
+output.start()
