43a15cfc62de940a2ae0dd181c2bb1501349d63f
Stefano Rasi <rasi.stefano@gmail.com>
2023-07-01 21:14:19 +0200

    Refactoring

--- synth/curves/cubic_spline.py
+++ synth/curves/cubic_spline.py
@@ -2,14 +2,14 @@ import scipy
 
 import numpy as np
 
-class CubicSplineCurve:
+class CubicSpline:
     def __init__(self, points):
         self.x = np.array(points)[:,0]
         self.y = np.array(points)[:,1]
 
         self.spline = scipy.interpolate.CubicSpline(self.x, self.y)
 
-    def evaluate(self, length):
+    def interpolate(self, length):
         xs = np.linspace(0, self.x[-1], length)
 
         return self.spline(xs)
