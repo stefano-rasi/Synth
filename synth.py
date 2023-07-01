4bb1db5abbba05b79a20a3707e81cab1e6dd54fd
Stefano Rasi <rasi.stefano@gmail.com>
2023-07-01 14:24:10 +0200

    Command line arguments

--- synth.py
+++ synth.py
@@ -2,33 +2,61 @@ import os
 import sys
 import time
 
+import pprint
+import argparse
 import importlib
 
 sys.path.insert(0, 'synth')
 
+from midi_input import MidiInput
 from audio_output import AudioOutput
 from keyboard_input import KeyboardInput
 
-SAMPLE_RATE = 44100
+parser = argparse.ArgumentParser(prog='synth')
 
-path = sys.argv[1]
+parser.add_argument('path', nargs='?')
+
+parser.add_argument('-s', '--sample_rate', type=int, default=44100)
+parser.add_argument('-i', '--input_device', type=int)
+parser.add_argument('-o', '--output_device', type=int, default=9)
+
+parser.add_argument('-I', '--input_devices', action='store_true')
+parser.add_argument('-O', '--output_devices', action='store_true')
+
+args = parser.parse_args()
+
+pp = pprint.PrettyPrinter()
+
+if args.input_devices:
+    pp.pprint(MidiInput.devices())
+
+    quit()
+
+if args.output_devices:
+    pp.pprint(AudioOutput.devices())
+
+    quit()
+
+path = args.path
+
+sample_rate = args.sample_rate
+input_device = args.input_device
+output_device = args.output_device
+
+if args.input_device:
+    midi_input = MidiInput(args.input_device)
+else:
+    midi_input = KeyboardInput()
+
+audio_output = AudioOutput(output_device, sample_rate)
 
 mtime = os.path.getmtime(path)
-
-if len(sys.argv) > 2:
-    device = sys.argv[2]
-else:
-    device = 4
 
 name = path.strip('.\\').strip('.py').replace('\\', '.')
 
 module = importlib.import_module(name)
 
-midi_input = KeyboardInput()
-
-audio_output = AudioOutput(device)
-
-module.Sound(midi_input, audio_output, SAMPLE_RATE)
+module.Sound(midi_input, audio_output, sample_rate)
 
 audio_output.stream.start_stream()
 
@@ -42,6 +70,6 @@ while audio_output.stream.is_active():
 
         importlib.reload(module)
 
-        module.Sound(midi_input, audio_output, SAMPLE_RATE)
+        module.Sound(midi_input, audio_output, sample_rate)
     else:
         time.sleep(0.1)
