6e1981ae8c143054b4550745c2e6aa4f8b610179
Stefano Rasi <rasi.stefano@gmail.com>
2023-07-01 21:14:13 +0200

    MIDI utilities, clean up

--- synth/utils.py
+++ synth/utils.py
@@ -1,7 +1,21 @@
-def midi_to_frequency(note):
+MIDI_NOTES = {
+    'C': 0, 'D': 2, 'E': 4,
+    'F': 5, 'G': 7, 'A': 9, 'B': 11
+}
+
+def note_to_midi(name):
+    note = name[0]
+    octave = int(name[1])
+
+    return 12 * (octave + 1) + MIDI_NOTES[note]
+
+def note_to_pitch(name):
+    return midi_to_pitch(note_to_midi(name))
+
+def midi_to_pitch(note):
     return 440 * pow(2, (note - 69) / 12)
 
-def power_to_amplitude(samples, min=-100):
+def power_to_amplitude(samples, min=-60):
     decibels = samples * (-min) + min
 
     return (10 ** (decibels / 20))
