66bc6214c9a9f4ccec45e0da961d08f4b48d6f47
Stefano Rasi <rasi.stefano@gmail.com>
2023-07-01 21:13:23 +0200

    List devices

--- synth.py
+++ synth.py
@@ -12,20 +12,20 @@ from midi_input import MidiInput
 from audio_output import AudioOutput
 from keyboard_input import KeyboardInput
 
+pp = pprint.PrettyPrinter()
+
 parser = argparse.ArgumentParser(prog='synth')
 
 parser.add_argument('path', nargs='?')
 
 parser.add_argument('-s', '--sample_rate', type=int, default=44100)
 parser.add_argument('-i', '--input_device', type=int)
-parser.add_argument('-o', '--output_device', type=int, default=9)
+parser.add_argument('-o', '--output_device', type=int)
 
 parser.add_argument('-I', '--input_devices', action='store_true')
 parser.add_argument('-O', '--output_devices', action='store_true')
 
 args = parser.parse_args()
-
-pp = pprint.PrettyPrinter()
 
 if args.input_devices:
     pp.pprint(MidiInput.devices())
@@ -43,7 +43,7 @@ sample_rate = args.sample_rate
 input_device = args.input_device
 output_device = args.output_device
 
-if args.input_device:
+if args.input_device is not None:
     midi_input = MidiInput(args.input_device)
 else:
     midi_input = KeyboardInput()
