aece8c392955060b63928629c82fd2f224882963
Stefano Rasi <rasi.stefano@gmail.com>
2023-07-05 19:53:06 +0200

    Formant waveform

---
+++ synth/waveforms/formant.py
@@ -0,0 +1,31 @@
+import scipy
+
+import numpy as np
+
+class FormantWaveform:
+    def __init__(self, formant, sample_rate, harmonics=None):
+        self.harmonics = harmonics
+
+        self.formant = formant.formant(sample_rate)
+
+    def waveform(self, pitch, sample_rate):
+        max_freq = int(round((sample_rate / pitch / 2))) - 1
+
+        if self.harmonics is None:
+            harmonics = np.repeat(1, max_freq)
+        else:
+            harmonics = self.harmonics[:max_freq]
+
+        wavelength = int(round(sample_rate / pitch * 2))
+
+        harmonics_fft = np.zeros(wavelength)
+
+        harmonics_indexes = 4 * np.arange(len(harmonics), dtype=int)
+
+        harmonics_frequencies = pitch * np.arange(1, len(harmonics) + 1)
+
+        harmonics_fft[harmonics_indexes] = self.formant[harmonics_frequencies.astype(int)] * harmonics
+
+        waveform = scipy.fftpack.irfft(harmonics_fft) * (wavelength / 2)
+
+        return waveform
