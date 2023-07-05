818523b700adea8e1a1661df1c95695a56c38e8a
Stefano Rasi <rasi.stefano@gmail.com>
2023-07-05 19:34:40 +0200

    Formant

---
+++ synth/formant.py
@@ -0,0 +1,37 @@
+import scipy
+
+import numpy as np
+
+class Formant:
+    def __init__(self, curve, min_freq=20, max_freq=20000, log=False):
+        self.log = log
+
+        self.curve = curve
+
+        self.min_freq = min_freq
+        self.max_freq = max_freq
+
+    def formant(self, sample_rate):
+        ys = self.curve
+
+        xs = np.linspace(0, 1, len(self.curve))
+
+        min_freq = self.min_freq
+        max_freq = self.max_freq
+
+        if self.log:
+            min_freq_log = log(2, min_freq)
+
+            freq_range = log(2, max_freq) - log(base, min_freq)
+
+            freq_xs = np.power(2, xs * freq_range + min_freq_log) / max_freq
+        else:
+            freq_range = max_freq - min_freq
+
+            freq_xs = (xs * freq_range + min_freq) / max_freq
+
+        interpolate = scipy.interpolate.interp1d(freq_xs, ys, fill_value='extrapolate')
+
+        formant = interpolate(np.linspace(0, 1, int(round(sample_rate / 2))))
+
+        return formant
