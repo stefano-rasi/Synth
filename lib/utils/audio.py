42e9dd898f2c1a36942d7882690dc1c263ea8c0b
Stefano Rasi <rasi.stefano@gmail.com>
2023-08-11 20:23:14 +0200

    Add DB to amplitude function

--- lib/utils/audio.py
+++ lib/utils/audio.py
@@ -1,4 +1,7 @@
+def db_to_amplitude(db):
+    return (10 ** (db / 20))
+
 def power_to_amplitude(samples, min_db=-100):
     db = samples * (-min_db) + min_db
 
-    return (10 ** (db / 20))
+    return db_to_amplitude(db)
